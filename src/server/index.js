6ce12343e1b87d3afe5a08186ab117ab192d0fa1
Andrea Scacchi <andreascacchi10@gmail.com>
2021-03-26 18:25:49 +0100

    Update index.js

--- src/server/index.js
+++ src/server/index.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+require('dotenv').config();  
 const express = require('express');
 const bodyParser = require('body-parser');
 const fetch = require('node-fetch');
@@ -6,62 +6,37 @@ const path = require('path');
 
 const app = express();
 const port = 3000;
-const API_KEY = process.env.API_KEY;
-
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(port, () => console.log(`Mars Rovers app listening on port ${port}!`));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 app.use('/', express.static(path.join(__dirname, '../public')));
 
-// Curiosity API call
-app.get('/curiosity', async (req, res) => {
+
+// API call
+
+app.get('/:name', async (req, res) => {
+    const name = req.params.name.toLowerCase();
+
+    let date;
+    let url;
+    if (name === 'spirit') {
+        date = '2010-02-01'
+        url = `https://api.nasa.gov/mars-photos/api/v1/rovers/${name}/photos?earth_date=${date}&api_key=${process.env.API_KEY}`;
+    } else if (name === 'opportunity') {
+        date = '2018-06-04'
+        url = `https://api.nasa.gov/mars-photos/api/v1/rovers/${name}/photos?earth_date=${date}&api_key=${process.env.API_KEY}`;
+    } else if (name === 'curiosity') {
+        url = `https://api.nasa.gov/mars-photos/api/v1/rovers/${name}/latest_photos?api_key=${process.env.API_KEY}`;
+    } else {
+        res.send('Invalid address. Choose /opportunity, /spirit or /curiosity');
+        return
+    };
+
     try {
-        const date = new Date()
-        console.log(date)
-        let image = await fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/latest_photos?api_key=${process.env.API_KEY}`)
+        const results = await fetch(url)
             .then(res => res.json())
-        res.send({ image })
-    } catch (err) {
-        console.log('error', err);
-    }
-})
-
-
-// Opportunity API call
-app.get('/opportunity', async (req, res) => {
-    try{
-        const date = new Date()
-        console.log(date)
-        let image = await fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/opportunity/latest_photos?api_key=${process.env.API_KEY}`)
-            .then(res => res.json())
-        res.send({ image })
-    } catch (err) {
-        console.log('error', err);
-    }
-})
-
-
-// Spirit API call
-app.get('/spirit', async (req, res) => {
-    try{
-        const date = new Date()
-        console.log(date)
-        let image = await fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/spirit/latest_photos?api_key=${process.env.API_KEY}`)
-            .then(res => res.json())
-        res.send({ image })
-    } catch (err) {
-        console.log('error', err);
-    }
-})
-
-
-// example API call
-app.get('/apod', async (req, res) => {
-    try {
-        let image = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${process.env.API_KEY}`)
-            .then(res => res.json())
-        res.send({ image })
+        res.send({ results })
     } catch (err) {
         console.log('error:', err);
     }
-})
+});
